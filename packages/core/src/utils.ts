8351d347bc8e2bf8676318a1a02bc10ed80e992d
Sasha Gerrand <sasha@easol.com>
2023-06-19 12:37:01 +0100

    Update syntax from suggestion in code review
    As suggested by @kamilkisiela in code review, thanks Kamil!
    
    https://github.com/kamilkisiela/split-tests/pull/13#pullrequestreview-1485974370

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,20 +1,15 @@
 import fs from "fs";
+import { XMLParser } from "fast-xml-parser";
 import { TimeReport, Test } from "./types";
-
-const {XMLParser} = require("fast-xml-parser");
 
 export function loadXML(file: string) {
   const junitRaw = fs.readFileSync(file, "utf-8");
-  const parser = new XMLParser();
-
-  return parser.parse(
-    junitRaw,
-    {
+  const parser = new XMLParser({
       ignoreAttributes: false,
       attributeNamePrefix: "",
-    },
-    true
-  );
+    });
+
+  return parser.parse(junitRaw);
 }
 
 export function addNewFiles(reports: TimeReport[], tests: Test[]) {
